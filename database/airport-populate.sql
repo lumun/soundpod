75efb98034016a948c0cb017b9de4b71445d0ec7
Luke Janssen <ljanssen@ups.edu>
2015-03-27 14:11:25 -0700

    Removes db, adds sql files

---
+++ database/airport-populate.sql
@@ -0,0 +1,42 @@
+
+
+--populate passengers relation using one multiple-tuple insert statement
+insert into passengers values ('Homer', 'J', 'Simpson', '111-11-1111');
+insert into passengers values ('Bart', 'H', 'Simpson', '444-44-4444');
+insert into passengers values ('Lisa', 'G', 'Simpson', '222-22-2222');
+insert into passengers values ('Frank', NULL, 'Lovejoy', '555-55-5555');
+insert into passengers values ('Robert', 'N', 'Quimby', '666-66-6666');
+insert into passengers values ('Ned', 'T', 'Flanders', '777-77-7777');
+insert into passengers values ('Frank', NULL, 'Ryerson', '333-33-3333');
+
+
+
+--populate plane relation one multiple-tuple insert statement
+insert into plane values (NULL, 'Boeing', '747', 525, 570);
+insert into plane values (NULL, 'Boeing', '747', 525, 570);
+insert into plane values (NULL, 'Airbus', 'A350', 270, 580);
+insert into plane values (NULL, 'McDonnel Douglas', 'DC10', 380, 610);
+insert into plane values (NULL, 'Airbus', 'A380', 200, 500);
+
+
+--populate flight relation using multiple insert statements
+insert into flight values (NULL, 'Springfield, IL', '7:15', 'Chicago, IL', '7:45', 4);
+insert into flight values (NULL, 'Columbus, OH', '16:00', 'Portland, OR', '22:00', 4);
+insert into flight values (NULL, 'New York, NY', '12:30', 'Miami, FL', '13:00', 2);
+insert into flight values (NULL, 'Paris, France', '15:00', 'Munich, Germany', '17:40', 1);
+insert into flight values (NULL, 'Hartford, CT', '9:00', 'Phoenix, AZ', '12:00', 1);
+insert into flight values (NULL, 'Miami, FL', '10:45', 'Austin, TX', '13:30', 2);
+insert into flight values (NULL, 'Akron, OH', '14:20', 'Hartford, CT', '16:45', 3);
+
+
+--populate onboard relation using multiple insert statements
+insert into onboard values ('555-55-5555',6, '32F');	--Quimby: Miami -> Austin (flight 6)
+insert into onboard values ('111-11-1111',2, '2B');
+insert into onboard values ('222-22-2222',4, '1F');
+insert into onboard values ('777-77-7777',5, '25A');
+insert into onboard values ('333-33-3333',3, '25A');
+insert into onboard values ('666-66-6666',1, '30C');
+insert into onboard values ('444-44-4444',7, '30C');
+insert into onboard values ('777-77-7777',3, '25A');
+insert into onboard values ('555-55-5555',4, '25A');
+

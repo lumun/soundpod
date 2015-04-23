032f5b3184b62a302505e639e597f0d6a7457cfe
Graham Baker <graham.cr.baker@gmail.com>
2015-04-23 10:35:54 -0700

    adding boilerplate stuff

--- forums.php
+++ forums.php
@@ -1,7 +1,49 @@
-<?php include '_header.php'; ?>
+<?php include '_header.php'; 
 
 
+// we need to pull posts from database to fill this page, 
+?>
 
+
+<div class="container">
+	<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
+		<h1>Forums<br><small>Talk to Each Other!</small></h1>
+	</div>
+	<hr>
+</div>
+
+<div class="container">
+	<div class="well col-xs-6 col-sm-6 col-md-6 col-lg-6">
+		<a href=""><h4>Station Discussion</h4></a>
+		<p class="text-left">What is happening in KUPS? Have your say!</p>
+	</div>
+	<div class="well col-xs-4 col-sm-4 col-md-4 col-lg-4">
+		<h4>Last Post Wednesday 10:25am</h4>
+		<p class="text-left">By Graham Baker</p>
+	</div>
+</div>
+
+<div class="container">
+	<div class="well col-xs-6 col-sm-6 col-md-6 col-lg-6">
+		<a href=""><h4>Alternative</h4></a>
+		<p class="text-left">What is happening in the Alt World?</p>
+	</div>
+	<div class="well col-xs-4 col-sm-4 col-md-4 col-lg-4">
+		<h4>Last Post Monday 8:44pm</h4>
+		<p class="text-left">By Graham Baker</p>
+	</div>
+</div>
+
+<div class="container">
+	<div class="well col-xs-6 col-sm-6 col-md-6 col-lg-6">
+		<a href=""><h4>Hip Hop</h4></a>
+		<p class="text-left">What is happening in the Hip Hop World?</p>
+	</div>
+	<div class="well col-xs-4 col-sm-4 col-md-4 col-lg-4">
+		<h4>Last Post Wednesday 3:33pm</h4>
+		<p class="text-left">By Graham Baker</p>
+	</div>
+</div>
 
 
 <?php include '_footer.php'; ?>

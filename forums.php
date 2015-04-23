750feede408e7ad6d6252adace86014583a4a91b
Zeke Rosenberg <zrosenberg@pugetsound.edu>
2015-04-23 13:43:51 -0700

    removed some excess php from forums.php

--- forums.php
+++ forums.php
@@ -1,27 +1,29 @@
-<?php include '_header.php'; 
+<?php include '_header.php'; ?>
 
-
+<div class="container">
+<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
+		<h1>Forums<br><small>Talk to Each Other!</small></h1>
+	</div>
+	<hr>
+</div>
+<?php 
 // we need to pull posts from database to fill this page, 
 try
 {
 $db = new PDO('mysql:database/kups.mysql')//TODO: we need to put the mysql db stuff in here
 
-echo '<div class="container">'
-echo '<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
-		<h1>Forums<br><small>Talk to Each Other!</small></h1>
-	</div>
-	<hr>
-</div>'
-
 $genResult = $db -> query ("SELECT * FROM post where category == Station");
-echo '<div class="container">
+?>
+
+<div class="container">
 	<div class="well col-xs-6 col-sm-6 col-md-6 col-lg-6">
 		<a href=""><h4>Station Discussion</h4></a>
 		<p class="text-left">What is happening in KUPS? Have your say!</p>
 	</div>';
-echo '<div class="well col-xs-4 col-sm-4 col-md-4 col-lg-4">
-		<h4>Last Post Wednesday 10:25am</h4>'
-		
+<div class="well col-xs-4 col-sm-4 col-md-4 col-lg-4">
+		<h4>Last Post Wednesday 10:25am</h4>
+
+<?php
 foreach ($genResult as $tuple)
 {
 	$content = $tuple['content'];
@@ -32,18 +34,21 @@ foreach ($genResult as $tuple)
 	echo '<p class="text-left">Posted at ' .$tuple['time'].' </p>';
 
 }
+?>
 
-echo '</div> </div>';
-
-
-$altResult = $db -> query("SELECT * FROM post where category == Alt");
+</div> </div>
 
 
-echo '<div class="container">
+
+<div class="container">
 	<div class="well col-xs-6 col-sm-6 col-md-6 col-lg-6">
 		<a href=""><h4>Alternative</h4></a>
 		<p class="text-left">What is happening in the Alt World?</p>
-	</div>';
+	</div>
+
+<?php
+$altResult = $db -> query("SELECT * FROM post where category == Alt");
+
 
 foreach ($altResult as $tuple)
 {
@@ -55,15 +60,18 @@ foreach ($altResult as $tuple)
 	echo '<p class="text-left">Posted at ' .$tuple['time'].' </p>';
 
 }
-echo'</div></div>';
+?>
+</div></div>
 	
 	
-echo'<div class="container">
+<div class="container">
 	<div class="well col-xs-6 col-sm-6 col-md-6 col-lg-6">
 		<a href=""><h4>Hip Hop</h4></a>
 		<p class="text-left">What is happening in the Hip Hop World?</p>
 	</div>
-	<div class="well col-xs-4 col-sm-4 col-md-4 col-lg-4">';
+	<div class="well col-xs-4 col-sm-4 col-md-4 col-lg-4">
+
+<?php
 	$hipResult = $db -> query("SELECT * FROM post where category == Hip");
 foreach ($hipResult as $tuple)
 {
@@ -75,8 +83,10 @@ foreach ($hipResult as $tuple)
 	echo '<p class="text-left">Posted at ' .$tuple['time'].' </p>';
 
 }
-echo'</div></div>';
+?>
+</div></div>
 
+<?php
 }catch(PDOException $e)
 {
 	print 'Exception : '.$e -> getMessage();

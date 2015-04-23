d9a29bf99c861033baec2701935ff2299373fccd
Zeke Rosenberg <zrosenberg@pugetsound.edu>
2015-04-23 13:34:59 -0700

    added changes to forum.php so it is generated dynamically (almost)

--- forums.php
+++ forums.php
@@ -2,48 +2,85 @@
 
 
 // we need to pull posts from database to fill this page, 
-?>
+try
+{
+$db = new PDO('mysql:database/kups.mysql')//TODO: we need to put the mysql db stuff in here
 
-
-<div class="container">
-	<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
+echo '<div class="container">'
+echo '<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
 		<h1>Forums<br><small>Talk to Each Other!</small></h1>
 	</div>
 	<hr>
-</div>
+</div>'
 
-<div class="container">
+$genResult = $db -> query ("SELECT * FROM post where category == Station");
+echo '<div class="container">
 	<div class="well col-xs-6 col-sm-6 col-md-6 col-lg-6">
 		<a href=""><h4>Station Discussion</h4></a>
 		<p class="text-left">What is happening in KUPS? Have your say!</p>
-	</div>
-	<div class="well col-xs-4 col-sm-4 col-md-4 col-lg-4">
-		<h4>Last Post Wednesday 10:25am</h4>
-		<p class="text-left">By Graham Baker</p>
-	</div>
-</div>
+	</div>';
+echo '<div class="well col-xs-4 col-sm-4 col-md-4 col-lg-4">
+		<h4>Last Post Wednesday 10:25am</h4>'
+		
+foreach ($genResult as $tuple)
+{
+	$content = $tuple['content'];
+	$uid = $tuple['uid'];
+	$user = $db -> query("SELECT * from user where uid == $uid");
+	echo '<p class="text-left">'.$content.' </p>';
+	echo '<p class="text-left">By' .$user['name'].' </p>';
+	echo '<p class="text-left">Posted at ' .$tuple['time'].' </p>';
 
-<div class="container">
+}
+
+echo '</div> </div>';
+
+
+$altResult = $db -> query("SELECT * FROM post where category == Alt");
+
+
+echo '<div class="container">
 	<div class="well col-xs-6 col-sm-6 col-md-6 col-lg-6">
 		<a href=""><h4>Alternative</h4></a>
 		<p class="text-left">What is happening in the Alt World?</p>
-	</div>
-	<div class="well col-xs-4 col-sm-4 col-md-4 col-lg-4">
-		<h4>Last Post Monday 8:44pm</h4>
-		<p class="text-left">By Graham Baker</p>
-	</div>
-</div>
+	</div>';
 
-<div class="container">
+foreach ($altResult as $tuple)
+{
+	$content = $tuple['content'];
+	$uid = $tuple['uid'];
+	$user = $db -> query("SELECT * from user where uid == $uid");
+	echo '<p class="text-left">'.$content.' </p>';
+	echo '<p class="text-left">By' .$user['name'].' </p>';
+	echo '<p class="text-left">Posted at ' .$tuple['time'].' </p>';
+
+}
+echo'</div></div>';
+	
+	
+echo'<div class="container">
 	<div class="well col-xs-6 col-sm-6 col-md-6 col-lg-6">
 		<a href=""><h4>Hip Hop</h4></a>
 		<p class="text-left">What is happening in the Hip Hop World?</p>
 	</div>
-	<div class="well col-xs-4 col-sm-4 col-md-4 col-lg-4">
-		<h4>Last Post Wednesday 3:33pm</h4>
-		<p class="text-left">By Graham Baker</p>
-	</div>
-</div>
+	<div class="well col-xs-4 col-sm-4 col-md-4 col-lg-4">';
+	$hipResult = $db -> query("SELECT * FROM post where category == Hip");
+foreach ($hipResult as $tuple)
+{
+	$content = $tuple['content'];
+	$uid = $tuple['uid'];
+	$user = $db -> query("SELECT * from user where uid == $uid");
+	echo '<p class="text-left">'.$content.' </p>';
+	echo '<p class="text-left">By' .$user['name'].' </p>';
+	echo '<p class="text-left">Posted at ' .$tuple['time'].' </p>';
 
+}
+echo'</div></div>';
+
+}catch(PDOException $e)
+{
+	print 'Exception : '.$e -> getMessage();
+}
+?>
 
 <?php include '_footer.php'; ?>

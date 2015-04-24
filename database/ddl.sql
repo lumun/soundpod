c87edafdb4f795ed3120289cb676bdc31278ce4a
Zeke Rosenberg <zrosenberg@pugetsound.edu>
2015-04-23 19:41:32 -0700

    edited sign up php file to be relevant to our user account, changed ddl to make email primary key for users and got rid of userid.

--- database/ddl.sql
+++ database/ddl.sql
@@ -1,24 +1,23 @@
 CREATE TABLE user {
-	uid INT NOT NULL,
-	name VARCHAR(20) NOT NULL,
+	name TEXT NOT NULL,
 	admin BOOLEAN DEFAULT 0,
-	password VARCHAR(20) NOT NULL.
-	email VARCHAR(30) NOT NULL,
-	PRIMARY KEY (uid)
+	passwrd TEXT NOT NULL.
+	email TEXT NOT NULL,
+	PRIMARY KEY (email)
 }
 
 CREATE TABLE show {
 	showid INT NOT NULL.
-	genre VARCHAR(20),
-	semester VARCHAR(8),
+	genre TEXT,
+	semester TEXT,
 	PRIMARY KEY showid
 }
 
 CREATE TABLE dj {
-	uid INT,
+	email TEXT,
 	showid INT,
-	PRIMARY KEY (uid, showid),
-	FOREIGN KEY (uid) REFERENCES user(uid)
+	PRIMARY KEY (email, showid),
+	FOREIGN KEY (email) REFERENCES user(email)
 		ON DELETE CASCADE,
 	FOREIGN KEY (showid) REFERENCES show(showid)
 		ON DELETE CASCADE
@@ -26,7 +25,7 @@ CREATE TABLE dj {
 
 CREATE TABLE showInstance {
 	showid INT,
-	day VARCHAR(10),
+	day TEXT,
 	time INT,
 	PRIMARY KEY (showid, day, time),
 	FOREIGN KEY (showid) REFERENCES show(showid)
@@ -47,18 +46,22 @@ CREATE TABLE subRequest {
 }
 
 CREATE TABLE category {
-	name VARCHAR(20) NOT NULL,
+	name TEXT NOT NULL,
 	PRIMARY KEY (name)
 }
 
 CREATE TABLE post {
 	postid INT NOT NULL,
-	uid INT NOT NULL,
+	email TEXT NOT NULL,
 	time TIMESTAMP,
-	content VARCHAR(1000) NOT NULL,
-	category VARCHAR(30) NOT NULL,
+	content TEXT NOT NULL,
+	category TEXT NOT NULL,
 	PRIMARY KEY (postid),
 	FOREIGN KEY (category) REFERENCES category(name)
 		ON UPDATE CASCADE
 		ON DELETE CASCADE
+	FOREIGN KEY(email) REFERENCES user(email)
+		ON UPDATE CASCADE
+		ON DELETE CASCADE	
+
 }
--- sign-up.php
+++ sign-up.php
@@ -1,28 +1,19 @@
 <?php
 // security and error checking
 // define variables and set to empty values
-$f_name = $l_name = $email = $username = $password = "";
-$f_nameErr = $l_nameErr = $emailErr = $usernameErr = $passwordErr = "";
+$name = $email  = $password = "";
+$emailErr = $usernameErr = $passwordErr = "";
 
 include '_helpers.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  	if (empty($_POST["f_name"])) {
-  		$f_nameErr = "Name is required";
+  	if (empty($_POST["name"])) {
+  		$nameErr = "Name is required";
   	} 
   	else {
-  		$f_name = clean_input($_POST["f_name"]);
-  		if (!preg_match("/^[a-zA-Z '-]+$/",$f_name)) {
-				$f_nameErr = "Only letters and white space allowed";
-		}
-	}
-  	if (empty($_POST["l_name"])) {
-  		$l_nameErr = "Name is required";
-  	} 
-  	else {				
-		$l_name = clean_input($_POST["l_name"]);
-  		if (!preg_match("/^[a-zA-Z '-]+$/",$l_name)) {
-				$l_nameErr = "Only letters and white space allowed";
+  		$name = clean_input($_POST["name"]);
+  		if (!preg_match("/^[a-zA-Z '-]+$/",$name)) {
+				$nameErr = "Only letters and white space allowed";
 		}
 	}
   	if (empty($_POST["email"])) {
@@ -32,15 +23,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
   		if (!preg_match("/^[A-Za-z0-9._%+-]+@(pugetsound\.edu|ups\.edu)$/",$email)) {
 				$emailErr = "Required format: username@pugetsound.edu";
-		}
-	}
-  	if (empty($_POST["username"])) {
-  		$usernameErr = "Username is required";
-  	} 
-  	else {
-  		$username = clean_input($_POST["username"]);
-  		if (!preg_match("/^[\w]+$/",$username)) {
-				$usernameErr = "Only letters and numbers allowed";
 		}
 	}
   	if (empty($_POST["password"])) {
@@ -55,23 +37,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 
 // Successful form submission is handled here
-if (!empty($f_name) AND !empty($l_name) AND !empty($email) AND !empty($username) AND !empty($password) AND empty($f_nameErr) AND empty($l_nameErr) AND empty($emailErr) AND empty($usernameErr) AND empty($passwordErr)) {
+if (!empty($name) AND !empty($email) AND !empty($password) AND empty($nameErr) AND empty($emailErr) AND empty($passwordErr)) {
 	// Attempt to insert the data
-	// try {
-	// 	$db = new PDO('sqlite:database/airport.sqlite3');
-	// 	$db -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	// 	$sql = "INSERT INTO passengers VALUES ('$f_name', '$m_name', '$l_name', '$email')";
-	// 	// insert
-	// 	$db -> exec($sql);
-	// 	// disconnect
-	// 	$db = NULL;
+	try {
+		//TODO::: fix this line to be relevant to OUR database AND mysql
+		$db = new PDO('sqlite:database/airport.sqlite3');
+		$db -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+		//everyone starts out as an admin, admins must promote them?
+		$sql = "INSERT INTO users VALUES ( ".$name.", 0, ".$password.", ".$email")";
+		// insert'
+		$db -> exec($sql);
+		// disconnect
+		$db = NULL;
 
 		//redirect to login page
-		// header('Location: /login.php');
-	// }
-	// catch(PDOException $e) {
-	// 	print 'Exception : '.$e -> getMessage();
-	// }
+		header('Location: /login.php');
+	}
+	catch(PDOException $e) {
+		print 'Exception : '.$e -> getMessage();
+	}
 }
 
 ?>
@@ -82,20 +67,20 @@ if (!empty($f_name) AND !empty($l_name) AND !empty($email) AND !empty($username)
     		<legend>Sign Up</legend>
     	
     		<div class="form-group">
-    			<label for="fname">First Name</label>
-    			<input type="text" placeholder="First Name" class="form-control" name="f_name" value="<?php echo $f_name;?>"><span class="input-error"> <?php echo $f_nameErr;?></span>
+    			<label for="name">First Name</label>
+    			<input type="text" placeholder="Name" class="form-control" name="name" value=<?php echo "\"".$name."\"";?>><span class="input-error"> <?php echo $f_nameErr;?></span>
     		</div>
 	    	<div class="form-group">
 	    		<label for="lname">Last Name</label>
-	    		<input type="text" placeholder="Last Name" class="form-control" name="l_name" value="<?php echo $l_name;?>"><span class="input-error" > <?php echo $l_nameErr;?></span>
+	    		<input type="text" placeholder="Last Name" class="form-control" name="l_name" value=<?php echo "\"".$l_name."\"";?>><span class="input-error" > <?php echo $l_nameErr;?></span>
 	    	</div>
 	    	<div class="form-group">
 	    		<label for="email">Email</label>
-	    		<input type="text" class="form-control" name="email" placeholder="you@pugetsound.edu" <?php if (!empty($email)) { echo "value=".$email; } ?> ><span class="input-error"> <?php echo $emailErr;?></span>
+	    		<input type="text" class="form-control" name="email" placeholder="you@pugetsound.edu" <?php if (!empty($email)) { echo "value=\"".$email."\""; } ?> ><span class="input-error"> <?php echo $emailErr;?></span>
 	    	</div>
 	    	<div class="form-group">
 	    		<label for="password">Password</label>
-	    		<input type="password" class="form-control" name="password" value="<?php echo $password;?>"><span class="input-error" > <?php echo $passwordErr;?></span>
+	    		<input type="password" class="form-control" name="password" value=<?php echo "\"".$password."\"";?>><span class="input-error" > <?php echo $passwordErr;?></span>
 	    	</div>
 
 	    	<div class='input-group date' >

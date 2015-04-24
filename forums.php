4d1666484cac165ccc17296d31321d2a760be1a2
Luke Janssen <ljanssen@ups.edu>
2015-04-24 14:44:28 -0700

    Small edits to forums and login

--- forums.php
+++ forums.php
@@ -8,43 +8,42 @@
 </div>
 <?php 
 // we need to pull posts from database to fill this page, 
-try
-{
-$db = new PDO('mysql:database/kups.mysql')//TODO: we need to put the mysql db stuff in here
+try {
+	//open the database
+	$db = new PDO("mysql:dbname=soundpod", 'root');
+	// Set errormode to exceptions
+	$db -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$categories = $db -> query("SELECT DISTINCT category From post");
-foreach($categories as $category)
-{
-	?>
-	<div class="container">
-	<div class="well col-xs-6 col-sm-6 col-md-6 col-lg-6">
+	$categories = $db -> query("SELECT DISTINCT category From post");
+	foreach($categories as $category)
+	{ ?>
+		<div class="container">
+		<div class="well col-xs-6 col-sm-6 col-md-6 col-lg-6">
+			<?php
+			echo '<a href=""><h4>'.$category.' Discussion</h4></a>';
+			echo '<p class="text-left">What is happening in '.$category.'? Have your say!</p>';
+			?>
+		</div>
+		<div class="well col-xs-4 col-sm-4 col-md-4 col-lg-4">
 		<?php
-		echo '<a href=""><h4>'.$category.' Discussion</h4></a>'
-		echo '<p class="text-left">What is happening in '.$category.'? Have your say!</p>'
-		?>
-	</div>';
-<div class="well col-xs-4 col-sm-4 col-md-4 col-lg-4">
-<?php
-	$result = $db -> query("SELECT * from post where category == ".$category);
-	foreach ($result as $tuple)
-	{
-		$content = $tuple['content'];
-		$uid = $tuple['uid'];
-		$user = $db -> query("SELECT * from user where uid == ". $uid);
-		echo '<p class="text-left">'.$content.' </p>';
-		echo '<p class="text-left">By' .$user['name'].' </p>';
-		echo '<p class="text-left">Posted at ' .$tuple['time'].' to '.$category'</p>';
+		$result = $db -> query("SELECT * from post where category == ".$category);
+		foreach ($result as $tuple)
+		{
+			$content = $tuple['content'];
+			$uid = $tuple['uid'];
+			$user = $db -> query("SELECT * from user where uid == ". $uid);
+			echo "<p class='text-left'>" . $content . "</p>";
+			echo "<p class='text-left'>By " . $user['name'] . "</p>";
+			echo "<p class='text-left'>Posted at " . $tuple['time'] . " to " . $category . "</p>";
 
+		}
+		// close the db
+		$db = NULL;	
+		echo "</div> </div>";
 	}
-?>
-</div> </div>
 }
-
-<?php
-}catch(PDOException $e)
-{
+catch(PDOException $e) {
 	print 'Exception : '.$e -> getMessage();
 }
-?>
 
-<?php include '_footer.php'; ?>
+include '_footer.php'; ?>
--- login.php
+++ login.php
@@ -3,40 +3,36 @@ include '_helpers.php';
 
 // Successful form submission is handled here
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	if (!empty($email) AND !empty($password) {
-	  	$email = clean_input($_POST["email"]);				
-		$password = clean_input($_POST["password"]);
+  	$email = clean_input($_POST["email"]);				
+	$password = clean_input($_POST["password"]);
 
-		// Attempt to match the data
-		try {
-		 	$db = new PDO("mysql:dbname=soundpod", 'root');
-		 	$db -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		 	$result = $db -> query ("SELECT uid,password FROM user WHERE email = $email");
-		 	if ($result) {
-		 		if ($result.password == $password) {
-		 				session_start();
-						session_regenerate_id(true);
-						$_SESSION["loggedin"] = "true";
-						$_SESSION["user"] = $result.uid;
-		 		}
-		 	}
+	// Attempt to match the data
+	try {
+	 	$db = new PDO("mysql:dbname=soundpod", 'root');
+	 	$db -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	 	$result = $db -> query ("SELECT uid,password FROM user WHERE email = $email");
+	 	if ($result) {
+	 		if ($result.password == $password) {
+	 				session_start();
+					session_regenerate_id(true);
+					$_SESSION["loggedin"] = "true";
+					$_SESSION["user"] = $result.uid;
+	 		}
+	 	}
 
-		 	// disconnect
-			$db = NULL;
+	 	// disconnect
+		$db = NULL;
 
-			if (isset($_SESSION["loggedin"])) {
-				header('Location: /account.php');
-			}
-			else {
-				echo "There was an error logging in";
-			}
+		if (isset($_SESSION["loggedin"])) {
+			header('Location: /account.php');
 		}
-		catch(PDOException $e) {
-		 	print 'Exception : '.$e -> getMessage();
+		else {
+			echo "There was an error logging in";
 		}
 	}
-
-
+	catch(PDOException $e) {
+	 	print 'Exception : '.$e -> getMessage();
+	}
 }
 ?>
 
@@ -49,8 +45,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 </head>
 
 <body>
-	<!-- faitwrapper class is necessary on ALL pages for sticky footer -->
-    <div id="faitwrapper">
     	<!-- Header inserter here -->
 		<?php 
 		include '_header.php';
@@ -60,7 +54,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 				<h2 style="text-decoration: underline">Login Form</h2>
 				<br/>
 				<form id="data-input" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-					<p>Email:</p> <input type="text" name="email" value="<?php echo $email;?>">
+					<p>Email:</p> <input type="text" name="email" value="<?php if (!empty($email)){echo $email;}?>">
 					<br/>
 					<p>Password:</p> <input type="text" name="password">
 					<br/>
@@ -72,11 +66,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 			echo "<h2 class='center small-caps'>YOU ARE ALREADY LOGGED IN</h2>";
 		}
 		?>
-
-		<div id="faitpush"></div>
-	</div>
-	<div id="faitfooter">
-		<?php include '_footer.php'; ?>
-	</div>
 </body>
 </html>

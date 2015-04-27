f974439ae6c05f41fc3fb8ec7c978ee5993052aa
Zeke Rosenberg <zrosenberg@pugetsound.edu>
2015-04-26 18:24:56 -0700

    added a submti button

--- _submit-post.php
+++ _submit-post.php
@@ -6,8 +6,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	$email = $_SESSION['email'];
 	$content = $_POST['content'];
 	$category = $_POST['category'];
-	$sql = "INSERT INTO post(email,content,category) values($email,$content,$category)";
-	echo $sql;
+	$sql = "INSERT INTO post(email,content,category) values('$email','$content','$category')";
+	//echo $sql;
 	try{
 	$db = new PDO("mysql:dbname=soundpod", 'root');
 	// Set errormode to exceptions
@@ -19,7 +19,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	}
 }
 
-//header("Location: /forums.php?category=$category");
-//die();
+header("Location: /forums.php?category=$category");
+die();
 
 ?>

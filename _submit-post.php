2d09de663f4332c891372a44819a3ce1fc31aa9c
Zeke Rosenberg <zrosenberg@pugetsound.edu>
2015-04-27 16:26:45 -0700

    Forums work! redirect fix~

--- _submit-post.php
+++ _submit-post.php
@@ -21,7 +21,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	}
 }
 
-//header("Location: /forums.php?category=$category");
-//die();
+header("Location: /forums.php?category=$category");
+die();
 
 ?>

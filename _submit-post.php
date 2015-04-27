3f7d67c8e6309e4cde0a6b1b7cd493b48587da89
Zeke Rosenberg <zrosenberg@pugetsound.edu>
2015-04-27 16:01:29 -0700

    ff

--- _submit-post.php
+++ _submit-post.php
@@ -3,11 +3,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	$email = $_SESSION['email'];
 	$content = $_POST['content'];
 	$category = $_POST['category'];
+	$cat = $_POST['categoryClean'];
 	try{
 	$db = new PDO("mysql:dbname=soundpod", 'root');
 
 	$content = $db->quote($content);
-	$sql = "INSERT INTO post(email,content,category) values('$email',$content,$category)";
+	$sql = "INSERT INTO post(email,content,category) values('$email',$content,$cat)";
 	// Set errormode to exceptionsgi
 	$db -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	$db->exec($sql);
--- forums.php
+++ forums.php
@@ -51,6 +51,7 @@ if($result->rowCount() < 1)
 	<input type="text" class="form-control" name="content" placeholder="Post here" width = "100px" height = "100px" >
 </div>
 
+<input type="hidden" name="categoryClean" value="<?php echo $cat; ?>" class="form-control">
 <input type="hidden" name="category" value="<?php echo $cat; ?>" class="form-control">
 
 <button  type="submit" class="btn btn-primary">Submit</button>

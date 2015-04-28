9f7916ffa1bdb06638ed2c8e36aae6fc64f4dd61
Zeke Rosenberg <zrosenberg@pugetsound.edu>
2015-04-27 17:43:55 -0700

    saving

--- _submit-post.php
+++ _submit-post.php
@@ -9,13 +9,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	$db = new PDO("mysql:dbname=soundpod", 'root');
 
 	$content = $db->quote($content);
-	//$cat = $db->quote($cat);
+	$cat = $db->quote($cat);
 	$sql = "INSERT INTO post(email,content,category) values('$email',$content,$cat)";
 	print($sql);
 	// Set errormode to exceptionsgi
 	$db -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	$db->exec($sql);
-	
+
 	$db=null;
 	}catch(PDOException $e) {
 	print 'Exception : '.$e -> getMessage();
